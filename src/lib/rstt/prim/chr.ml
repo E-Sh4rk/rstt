586651c7dc85d41a3cadb8632427518abaf12cee
Mickael Laurent <esh4rk@gmail.com>
2025-12-13 11:00:40 +0100

    typo

--- src/lib/rstt/prim/chr.ml
+++ src/lib/rstt/prim/chr.ml
@@ -54,10 +54,10 @@ module P = struct
     if pos then
       aux prec assoc fmt strs
     else if not pos && strs = [] then
-      Format.fprintf fmt "str"
+      Format.fprintf fmt "chr"
     else
       let sym,prec',_ as opinfo = binop_info Diff in
-      fprintf prec assoc opinfo fmt "str%s%a" sym (aux prec' Right) strs
+      fprintf prec assoc opinfo fmt "chr%s%a" sym (aux prec' Right) strs
 end
 
 include Na.MakeCompWithNa(P)
