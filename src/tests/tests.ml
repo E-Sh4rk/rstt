e34e73160f08d4d5c8990fe73eaf9292aa21c3a8
Mickael Laurent <esh4rk@gmail.com>
2026-05-30 10:18:32 +0200

    accept . in identifiers

--- src/tests/tests.ml
+++ src/tests/tests.ml
@@ -86,6 +86,7 @@ let%expect_test "tests" =
     class7: <class1, ?class3>
     class8: <class1, ~class2, ...>
     class9: <~class2, ?class3, *>
+    class10: <data.frame>
     attr1: int with { dim: int, any }
     attr2: int with { dim: int }
     attr3: int with ({ dim: int, any } & ~{ dim: int })
